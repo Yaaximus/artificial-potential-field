25428183dcc5156f0be415979609718be4ffb429
mostwanted999 <yasim.ahmed63@yahoo.com>
2019-05-15 17:13:54 +0500

    Plot parameters and properties defined(legends, markers, object color, size)

--- main.py
+++ main.py
@@ -14,12 +14,17 @@ from Robot import Robot
 from Object import Object
 import matplotlib.pyplot as plt
 
+
 def main():
     """
     This is the function for instantiating object of Robot, Goal, and Obstacles.
     This programs runs until distance of robot is less then 0.9 to goal and 
     during this process it keeps on deciding next move and then exectuing it.
     """
+
+    iteration_no = 1.0
+    x_limit = 20.0
+    y_limit = 20.0
 
     obstacle1 = Object(pos_x=9.0, pos_y=5.0, sigma=1.0)
     obstacle2 = Object(pos_x=9.0, pos_y=8.0, sigma=1.0)
@@ -30,24 +35,33 @@ def main():
     robot = Robot(pos_x=5.0, pos_y=5.0, sensor_range=2.0, npts=60)
 
     plt.figure()
-    plt.axis([0, 20, 0, 20])
-    
-    iter = 1
+    plt.axis([0.0, x_limit, 0.0, y_limit])
+
     while robot.get_distance_to_object(goal=goal) > 0.9:
 
-        plt.plot(robot.get_coordinate("x"), robot.get_coordinate("y"),"o")
-        plt.plot(obstacle1.get_coordinate("x"), obstacle1.get_coordinate("y"),"o")
-        plt.plot(obstacle2.get_coordinate("x"), obstacle2.get_coordinate("y"),"o")
-        plt.plot(obstacle3.get_coordinate("x"), obstacle3.get_coordinate("y"),"o")
-        plt.plot(obstacle4.get_coordinate("x"), obstacle4.get_coordinate("y"),"o")
-        plt.plot(goal.get_coordinate("x"), goal.get_coordinate("y"),"o")
+        plt.plot(robot.get_coordinate("x"), robot.get_coordinate(
+            "y"), "bo", markersize=x_limit/1.5)
+        plt.plot(goal.get_coordinate("x"), goal.get_coordinate(
+            "y"), "go", markersize=x_limit*goal.get_sigma())
+        plt.plot(obstacle1.get_coordinate("x"), obstacle1.get_coordinate(
+            "y"), "ro", markersize=x_limit*obstacle1.get_sigma())
+        plt.plot(obstacle2.get_coordinate("x"), obstacle2.get_coordinate(
+            "y"), "ro", markersize=x_limit*obstacle2.get_sigma())
+        plt.plot(obstacle3.get_coordinate("x"), obstacle3.get_coordinate(
+            "y"), "ro", markersize=x_limit*obstacle3.get_sigma())
+        plt.plot(obstacle4.get_coordinate("x"), obstacle4.get_coordinate(
+            "y"), "ro", markersize=x_limit*obstacle4.get_sigma())
+
+        plt.legend(('Robot', 'Goal', 'Obstacle'), loc='lower right',
+                   fontsize='small', numpoints=1, markerscale=0.5, labelspacing=1)
 
         robot.decide_next_move(goal=goal, obstacles=obstacles)
         robot.take_next_move()
         plt.draw()
-        plt.savefig("./images_for_gif/"+str(iter)+".png")
-        iter += 1
+        plt.savefig("./images_for_gif/"+str(iteration_no)+".png")
+        iteration_no += 1.0
         plt.pause(0.1)
+
 
 if __name__ == "__main__":
 
